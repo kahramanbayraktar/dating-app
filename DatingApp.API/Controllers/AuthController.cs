3ffc388f091207b9014de2a526766fd17f50b805
kahramanbayraktar <kahramanbayraktar@ite-turkey.com>
2019-02-11 23:37:59 +0300

    End of Section 6

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -62,7 +62,7 @@ namespace DatingApp.API.Controllers
 
             var tokenDescriptor = new SecurityTokenDescriptor
             {
-                Subject = new ClaimsIdentity(),
+                Subject = new ClaimsIdentity(claims),
                 Expires = DateTime.Now.AddDays(1),
                 SigningCredentials = creds,
             };
